d1a2ed9a8fed4790c4421d47bf8ed8deed445a58
Felipe Brandt <paulosbrandt@gmail.com>
2025-03-24 14:49:31 -0300

    - Permuta com Match - Imoveis de Interesse - Imóveis Interessados
    - Notificações de Permuta

--- db_connection.py
+++ db_connection.py
@@ -4,15 +4,15 @@ BASE = 'nuvem'
 
 
 if BASE == 'nuvem':
-    db = PostgresqlDatabase(database=getenv('DATABASE', 'defaultdb'),
-                            host=getenv('HOST', 'linkimovel-brain-ag.l.aivencloud.com'),
-                            port=getenv('PORT', '21242'),
-                            user=getenv('USER', 'avnadmin'),
-                            password=getenv('PASSWORD', 'AVNS_FLSIPqCapJPjpvmSHTG'))
+    db = PostgresqlDatabase(database=getenv('DATABASE', ''),
+                            host=getenv('HOST', ''),
+                            port=getenv('PORT', ''),
+                            user=getenv('USER', ''),
+                            password=getenv('PASSWORD', ''))
 else:
-    db = PostgresqlDatabase(database=getenv('DATABASE', 'linkimoveis'),
-                            host=getenv('HOST', 'localhost'),
-                            port=getenv('PORT', '5432'),
-                            user=getenv('USER', 'postgres'),
-                            password=getenv('PASSWORD', 'Pohgma@1980'))
+    db = PostgresqlDatabase(database=getenv('DATABASE', ''),
+                            host=getenv('HOST', ''),
+                            port=getenv('PORT', ''),
+                            user=getenv('USER', ''),
+                            password=getenv('PASSWORD', ''))
 
